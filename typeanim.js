d6e9fd4e16cab17a46b25131c96b6b21354a9d4f
sloast <adaadrobson@gmail.com>
2024-05-10 20:06:26 +0100

    refactor to use alpine & tailwind

--- typeanim.js
+++ typeanim.js
@@ -1,39 +1,31 @@
 
-let textarr = 'undefined'.split('')
-let textobj = null
-let currtext = ''
-let counter = 0
-let cursoractive = true
-let usecursor = false
-let running = true
+const usecursor = true
 const cursor = '_'
+const end = '&nbsp;';
 //const cursor = '&#x2588;'
 
-function typeletter() {
-    if (textarr.length > 0) {
-        currtext += textarr.shift()
-        textobj.innerHTML = currtext + ( usecursor ? cursor : '&nbsp;')
+function typeletter(obj) {
+    if (obj.textarr.length > 0) {
+        obj.currtext += obj.textarr.shift()
+        obj.innerHTML = obj.currtext + ( usecursor ? cursor : obj.suffixtext)
     } else if (usecursor) {
-        if (counter++ == 5) {
-            cursoractive = !cursoractive
-            textobj.innerHTML = currtext
-            + (cursoractive ? cursor : '&nbsp;')
-            counter = 0
+        if (obj.counter == undefined) obj.counter = 0;
+        if (obj.counter++ == 5) {
+            obj.cursoractive = !obj.cursoractive
+            obj.innerHTML = obj.currtext
+            + (obj.cursoractive ? cursor : obj.suffixtext)
+            obj.counter = 0
         }
         //typewriter.style.animation = 'blink-caret 1.5s step-end infinite'
     } else {
-        running = false
-        clearInterval();
+        clearInterval(obj.intervalId);
     }
 }
 
-window.onload = function() {
-    textobj = document.querySelector('#typewriter')
-    currtext = textobj.innerHTML
-    if (textobj != null) {
-        textarr = document.querySelector('#typewriter-text').value.split(''); //.map(x => x == ' ' ? '&nbsp;' : x)
-        setInterval(typeletter, 80);
-    }
+function startTypewriter(obj, text) {
+    obj.currtext = obj.innerHTML
+    obj.textarr = text.split(''); //.map(x => x == ' ' ? '&nbsp;' : x)
+    obj.suffixtext = '&nbsp;'
+    obj.cursoractive = true
+    obj.intervalId = setInterval(() => typeletter(obj), 90);
 }
-
-
