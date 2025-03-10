50fe789b8148e2c314be9e160ab4568cf0fd2512
sloast <adaadrobson@gmail.com>
2025-03-10 15:29:54 +0000

    mobile compatible header

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,10 @@
 <script lang="ts">
-  import Header from "./header.svelte";
-
-  let tttext: String = "";
-  let ttfinal: String = "adam robson";
-  let cursor: String = "_";
-
-  function add_char() {
-    tttext += ttfinal[0];
-    ttfinal = ttfinal.substring(1);
-    if (ttfinal.length === 0) {
-      clearInterval(id);
-      setInterval(() => {
-        cursor = cursor == "_" ? "\xa0" : "_";
-      }, 1000);
-    }
-  }
-
-  let id = 0;
-
-  setTimeout(() => {
-    id = setInterval(add_char, 100);
-  }, 200);
+  import Header from "./Header.svelte";
+  import Banner from "./Banner.svelte";
 </script>
 
 <Header></Header>
-<div
-  class="flex items-center justify-center relative min-h-72 overflow-clip z-0"
->
-  <h1 class="bg-gray-800 rounded p-4 pointer-events-none z-10 text-5xl">
-    &nbsp;{tttext}{cursor}
-  </h1>
-
-  <div id="background" class="inset-0 my-[-50px] absolute">
-    <iframe
-      src="https://www.shadertoy.com/embed/XfdSRs?gui=false&amp;t=10&amp;paused=false&amp;muted=true"
-      width="100%"
-      height="100%"
-      frameborder="0"
-      allowfullscreen={false}
-      title="3dperlinnoise"
-      class="inset-0"
-    ></iframe>
-  </div>
-</div>
+<Banner></Banner>
 <main class="pt-4">
   <p>
     welcome to my site. it's a bit empty at the moment, but i'm working on it :)
--- src/header.svelte
+++ src/header.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
     import slogo_full from "/slogo_full.svg";
+    import slogo from "/slogo.svg";
+
+    let innerWidth = 0;
+    let innerHeight = 0;
+    let rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    $: wide = innerWidth > 50 * rem;
 </script>
 
+<svelte:window bind:innerWidth bind:innerHeight />
+
 <header
-    class="sticky top-0 flex flex-row items-center justify-between border-b-gray-500 border-b-2 bg-gray-800 p-4 z-10"
+    class="sticky top-0 flex flex-row items-center justify-between border-b-gray-500 border-b-2 bg-gray-800 p-4 z-10 gap-4"
 >
     <a
-        class="basis-0 grow"
+        class="basis-0 grow min-w-[3em]"
         href="https://vite.dev"
         target="_blank"
         rel="noreferrer"
     >
-        <img src={slogo_full} class="logo" alt="Vite Logo" />
+        {#if wide}
+            <img src={slogo_full} class="logo" alt="Sloast Logo" />
+        {:else}
+            <img src={slogo} class="logo" alt="Sloast Logo" />
+        {/if}
     </a>
-    <div class="flex flex-row gap-4">
+    <div class="flex flex-row justify-end gap-4 flex-wrap">
         <!-- <a class="btn" href="game/">snake</a> -->
         <a class="btn" type="button" href="https://github.com/sloast/">github</a
         >
@@ -22,12 +34,17 @@
         </a>
         <a class="btn" type="button" href="/cubu">cubu</a>
     </div>
-    <p class="basis-0 grow text-right">&nbsp;[ made by human ]&nbsp;</p>
+    {#if wide}
+        <p class="basis-0 grow text-right min-w-[8em]">
+            &nbsp;[ made by human ]&nbsp;
+        </p>
+    {/if}
 </header>
 
 <style>
     .logo {
         height: 3em;
         filter: drop-shadow(0 0 1.5em #00e436);
+        aspect-ratio: auto;
     }
 </style>
