3ed619f58f52749e9577ffa63073259679359c6d
sloast <adaadrobson@gmail.com>
2025-03-10 14:35:58 +0000

    add name section

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,50 @@
 <script lang="ts">
-  import slogo from "/slogo.svg";
-  import slogo_full from "/slogo_full.svg";
-  import Counter from "./lib/Counter.svelte";
+  import Header from "./header.svelte";
+
+  let tttext: String = "";
+  let ttfinal: String = "adam robson";
+  let cursor: String = "_";
+
+  function add_char() {
+    tttext += ttfinal[0];
+    ttfinal = ttfinal.substring(1);
+    if (ttfinal.length === 0) {
+      clearInterval(id);
+      setInterval(() => {
+        cursor = cursor == "_" ? "\xa0" : "_";
+      }, 1000);
+    }
+  }
+
+  let id = 0;
+
+  setTimeout(() => {
+    id = setInterval(add_char, 100);
+  }, 200);
 </script>
 
-<header
-  class="sticky top-0 flex flex-row items-center justify-between border-b-gray-500 border-b-2 bg-gray-800 p-4"
+<Header></Header>
+<div
+  class="flex items-center justify-center relative min-h-72 overflow-clip z-0"
 >
-  <a
-    class="basis-0 grow"
-    href="https://vite.dev"
-    target="_blank"
-    rel="noreferrer"
-  >
-    <img src={slogo_full} class="logo" alt="Vite Logo" />
-  </a>
-  <div class="flex flex-row gap-4">
-    <!-- <a class="btn" href="game/">snake</a> -->
-    <a class="btn" type="button" href="https://github.com/sloast/">github</a>
-    <a class="btn" type="button" href="https://www.linkedin.com/in/sloast/">
-      linkedin
-    </a>
-    <a class="btn" type="button" href="/cubu">cubu</a>
-  </div>
-  <p class="basis-0 grow text-right">&nbsp;[ made by human ]&nbsp;</p>
-</header>
-<main>
-  <h1>Vite + Svelte</h1>
+  <h1 class="bg-gray-800 rounded p-4 pointer-events-none z-10 text-5xl">
+    &nbsp;{tttext}{cursor}
+  </h1>
 
-  <div class="card">
-    <Counter />
+  <div id="background" class="inset-0 my-[-50px] absolute">
+    <iframe
+      src="https://www.shadertoy.com/embed/XfdSRs?gui=false&amp;t=10&amp;paused=false&amp;muted=true"
+      width="100%"
+      height="100%"
+      frameborder="0"
+      allowfullscreen={false}
+      title="3dperlinnoise"
+      class="inset-0"
+    ></iframe>
   </div>
-
+</div>
+<main class="pt-4">
   <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
+    welcome to my site. it's a bit empty at the moment, but i'm working on it :)
   </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 3em;
-    filter: drop-shadow(0 0 1.5em #00e436);
-  }
-</style>
---
+++ src/header.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import slogo_full from "/slogo_full.svg";
+</script>
+
+<header
+    class="sticky top-0 flex flex-row items-center justify-between border-b-gray-500 border-b-2 bg-gray-800 p-4 z-10"
+>
+    <a
+        class="basis-0 grow"
+        href="https://vite.dev"
+        target="_blank"
+        rel="noreferrer"
+    >
+        <img src={slogo_full} class="logo" alt="Vite Logo" />
+    </a>
+    <div class="flex flex-row gap-4">
+        <!-- <a class="btn" href="game/">snake</a> -->
+        <a class="btn" type="button" href="https://github.com/sloast/">github</a
+        >
+        <a class="btn" type="button" href="https://www.linkedin.com/in/sloast/">
+            linkedin
+        </a>
+        <a class="btn" type="button" href="/cubu">cubu</a>
+    </div>
+    <p class="basis-0 grow text-right">&nbsp;[ made by human ]&nbsp;</p>
+</header>
+
+<style>
+    .logo {
+        height: 3em;
+        filter: drop-shadow(0 0 1.5em #00e436);
+    }
+</style>
